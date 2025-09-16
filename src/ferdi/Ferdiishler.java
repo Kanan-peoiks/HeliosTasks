dd9df61277a8aedba1abc545eb7a34473448a54c
Kanan-peoiks <kenanshukurzadee@gmail.com>
2025-09-16 13:43:37 +0400

    yenilendi

--- src/ferdi/Ferdiishler.java
+++ src/ferdi/Ferdiishler.java
@@ -4,13 +4,13 @@ public class Ferdiishler {
     public static void main (String[] args){
         int yas = 10;
 
-        yas = ++yas; //11 olur
+        // yas = ++yas; //11 olur
         System.out.println(yas);
-        yas = yas++; // əvvəlcə köhnə dəyəri qaytarır
+        // yas = yas++; // əvvəlcə köhnə dəyəri qaytarır
         System.out.println(yas);
-        yas = --yas;
+        // yas = --yas;
         System.out.println(yas);
-        yas = yas--;
+        // yas = yas--;
         System.out.println(yas);
     }
 }
