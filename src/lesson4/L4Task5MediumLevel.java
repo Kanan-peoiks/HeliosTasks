8fe2120c5c2437e009a9eaf7d96c6ff8962a2617
Kanan-peoiks <kenanshukurzadee@gmail.com>
2025-09-24 00:04:32 +0400

    Lesson4Task5MLprintlnchangeprintf

--- src/lesson4/L4Task5MediumLevel.java
+++ src/lesson4/L4Task5MediumLevel.java
@@ -2,13 +2,13 @@ package lesson4;
 
 public class L4Task5MediumLevel {
     public static void main(String[] args) {
-        int [] massiv = {0,10,20,30,40,50,60};
-        int cem =0;
-        int ortalama = 0;
+        float [] massiv = {0,10,20,30,40,50,60};
+        float cem =0;
+        float ortalama = 0;
         for (int i = 0; i< massiv.length; i++){
             cem += massiv[i];
             ortalama = cem / massiv.length;
         }
-        System.out.println(ortalama); //printf kimi duzelt
+        System.out.printf("Ortalama %.2f", ortalama); //printf kimi duzelt
     }
 }
