6c076dfd5e940d3cc997448ceb5e7eb268f43ca2
Kanan-peoiks <kenanshukurzadee@gmail.com>
2025-09-24 10:24:29 +0400

    lesson4exercise

--- src/lesson4/Lessson4.java
+++ src/lesson4/Lessson4.java
@@ -2,30 +2,30 @@ package lesson4;
 
 public class Lessson4 {
     public static void main(String[] args) {
-//        String[] sozler = {"Java", "Programlashdirma", "Dil", "Syntax"};
-//
-//        String enUzun = sozler[0];
-//
-//        for (String soz : sozler) {
-//            if (soz.length() > enUzun.length()) {
-//                enUzun = soz;
-//            }
-//        }
-//
-//        System.out.println("En uzun soz: " + enUzun);
+        String[] sozler = {"Java", "Programlashdirma", "Dil", "Syntax"};
+
+        String enUzun = sozler[0];
+
+        for (String soz : sozler) {
+            if (soz.length() > enUzun.length()) {
+                enUzun = soz;
+            }
+        }
+
+        System.out.println("En uzun soz: " + enUzun);
 
 
-//                int[][] arr = {
-//                        {10, 20, 66},
-//                        {30, 40, 88},
-//                        {50, 60, 99}};
-//                for (int[] row : arr) {
-//                    for (int element : row) {
-//                        System.out.print(element + " ");}
-//                        System.out.println();}
-        // Managing inventory across multiple stores
+                int[][] arr = {
+                        {10, 20, 66},
+                        {30, 40, 88},
+                        {50, 60, 99}};
+                for (int[] row : arr) {
+                    for (int element : row) {
+                        System.out.print(element + " ");}
+                        System.out.println();}
+
         //--------------------------------------------------------------------------------------------
-        /*
+        // Managing inventory across multiple stores
 // [store][product]
         int[][] storeInventory = {
                 {100, 150, 200},  // Store 1: Products A, B, C
@@ -60,7 +60,7 @@ public class Lessson4 {
             transactionBackup[i] = transactions[i];
             System.out.println(transactionBackup[i]);
         }
-*/
+
         double[] prices = {100, 105, 110, 120, 125, 130, 128, 132, 135, 140};
 
         // Son 3 günün ortasını hesablamaq üçün metod çağırırıq
