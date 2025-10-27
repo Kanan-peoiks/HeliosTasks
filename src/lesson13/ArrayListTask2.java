5b0f48c00acc97114365a59d3554e465879c95d4
Kanan-peoiks <kenanshukurzadee@gmail.com>
2025-10-27 20:44:45 +0400

    classtask

--- src/lesson13/ArrayListTask2.java
+++ src/lesson13/ArrayListTask2.java
@@ -1,4 +1,15 @@
 package lesson13;
 
+import java.util.ArrayList;
+
 public class ArrayListTask2 {
+    public static void main(String[] args) {
+        ArrayList<String> restoranYemekleri  = new ArrayList<>();
+        restoranYemekleri.add("DOLMA");
+        restoranYemekleri.add("Kartof");
+        restoranYemekleri.add("Plov");
+        System.out.println(restoranYemekleri.size());
+        restoranYemekleri.add("Qarabasaq");
+        System.out.println(restoranYemekleri.size());
+    }
 }
