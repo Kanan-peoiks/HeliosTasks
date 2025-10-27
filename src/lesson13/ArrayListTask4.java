3cf21fa75797362a8e55c58b161f01dc5c21b357
Kanan-peoiks <kenanshukurzadee@gmail.com>
2025-10-27 20:58:48 +0400

    classtask

--- src/lesson13/ArrayListTask4.java
+++ src/lesson13/ArrayListTask4.java
@@ -1,4 +1,24 @@
 package lesson13;
 
+import java.util.ArrayList;
+
 public class ArrayListTask4 {
+    public static void main(String[] args) {
+        ArrayList<Integer> dumble = new ArrayList<>();
+        dumble.add(5);
+        dumble.add(1);
+        dumble.add(25);
+        dumble.add(35);
+        dumble.add(45);
+        for (int i = 0; i < dumble.size(); i++) {
+            System.out.println(dumble.get(i));
+                    }
+        System.out.println("___________________________");
+        for (int i = 0; i < dumble.size(); i++) {
+            if (dumble.get(i) >=5 ){
+                System.out.println(dumble.get(i));
+            }
+        }
+
+    }
 }
