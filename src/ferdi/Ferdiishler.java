343738562de74eee77db207ffda6cb456653d9e5
Kanan-peoiks <kenanshukurzadee@gmail.com>
2025-10-31 18:56:37 +0400

    classtask

--- src/ferdi/Ferdiishler.java
+++ src/ferdi/Ferdiishler.java
@@ -5,6 +5,19 @@ import java.util.Stack;
 
 public class Ferdiishler {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        String binary = Integer.toBinaryString(n);
+        int max_count = 0;
+        String [] arr = binary.split("0");
+
+        for (int i = 0; i < arr.length; i++) {
+            int length = arr[i].length();
+            if (length > max_count){
+                max_count = length;
+            }
+        }
+        System.out.println(max_count);
 
 
             }
