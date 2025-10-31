00febefb3ee5c3a1908cfb477b9187a0bab8c782
Kanan-peoiks <kenanshukurzadee@gmail.com>
2025-10-31 20:40:05 +0400

    classtask

--- src/lesson14/HashSetTask2.java
+++ src/lesson14/HashSetTask2.java
@@ -1,4 +1,18 @@
 package lesson14;
 
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 public class HashSetTask2 {
+    public static void main(String[] args) {
+        Set<String> telebeler = new LinkedHashSet<>();
+telebeler.add("kenan");
+        telebeler.add("azer");
+        telebeler.add("senan");
+        telebeler.add("akif");
+        telebeler.add("kenan");
+        System.out.println(telebeler);
+
+    }
 }
