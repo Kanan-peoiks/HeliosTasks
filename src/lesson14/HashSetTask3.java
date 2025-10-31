fa2bf67fcb5e3b717e7578a3c807c532166fc91a
Kanan-peoiks <kenanshukurzadee@gmail.com>
2025-10-31 20:41:52 +0400

    classtask

--- src/lesson14/HashSetTask3.java
+++ src/lesson14/HashSetTask3.java
@@ -2,10 +2,18 @@ package lesson14;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class HashSetTask3 {
     public static void main(String[] args) {
-    Set<String> names = new HashSet<>();
+    Set<Integer> num = new TreeSet<>();
+    num.add(13);
+        num.add(12);
+        num.add(456);
+        num.add(12543);
+        num.add(4);
+        num.add(1365);
+        System.out.println(num);
 
     }
 
