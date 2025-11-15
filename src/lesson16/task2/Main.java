881a87f07ba2f1565b9100493c4e14461eeb69c4
Kanan-peoiks <kenanshukurzadee@gmail.com>
2025-11-16 02:39:45 +0400

    threadtask2

--- src/lesson16/task2/Main.java
+++ src/lesson16/task2/Main.java
@@ -4,23 +4,25 @@ import lesson8.task1.Trainable;
 
 public class Main {
     public static void main(String[] args) {
-    People p1 = new People("Akif");
-    People p2 = new People("Asif");
-    People p3 = new People("Arif");
-
-    p1.start();
-    p2.start();
-    p3.start();
+    People p1 = new People("Thread1","Akif");
+    People p2 = new People("Thread2","Asif");
+    People p3 = new People("Thread3","Arif");
 
     try {
+
+        p1.start();
         p1.join();
+
+        p2.start();
         p2.join();
+
+        p3.start();
         p3.join();
     }catch (InterruptedException e){
         Thread.currentThread().interrupt();
 
     }
-
+        System.out.println("Bütün müştərilər xidmət aldı.");
 
     }
 }
--- src/lesson16/task2/People.java
+++ src/lesson16/task2/People.java
@@ -3,14 +3,14 @@ package lesson16.task2;
 public class People extends Thread{
  private final String name;
 
-    public People(String name1) {
-
+    public People(String name, String name1) {
+        super(name);
         this.name = name1;
     }
 
     @Override
     public void run (){
-        System.out.println(Thread.currentThread().getName()+ " " + name +" kartdan pul çıxarır...");
+        System.out.println(name +" kartdan pul çıxarır...");
         try {
             Thread.sleep(1000);
         }
@@ -18,6 +18,7 @@ public class People extends Thread{
             Thread.currentThread().interrupt();
             return;
         }
+        System.out.println(name + " pul çıxardı və növbəni boşaltdı.");
 
     }
 
