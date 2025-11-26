4f60a96bd87beb316b69f37f6e20d0969b3b0dc6
Kanan-peoiks <kenanshukurzadee@gmail.com>
2025-11-27 02:49:54 +0400

    hackerrank2

--- src/ferdi/Ferdiishler.java
+++ src/ferdi/Ferdiishler.java
@@ -4,30 +4,37 @@ import java.util.HashMap;
 import java.util.Scanner;
 import java.util.Stack;
 
+
+    //SUBSTRING COMPARETO CHARACTER..
+
 public class Ferdiishler {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        String A = scan.nextLine();
-        String B = scan.nextLine();
+        String s = scan.nextLine();
+        int k = scan.nextInt();
+        // banana - > ban ana nan ana
+    int sum = s.length() - k + 1 ;
 
-        int res = A.length() + B.length();
-        System.out.println(res);
-        int res2 = A.compareTo(B);
-        if ( res2> 0){
-            System.out.println("Yes");
-        }
-        else{
-            System.out.println("No");
+        String [] arr = new String[sum];
+        for (int i = 0; i < sum; i++) {
+            arr[i] = s.substring(i,i+k);
         }
 
-        char Ab = A.charAt(0);
-        char ab = Character.toUpperCase(Ab);
+        String min = arr[0];
 
-        char Ba = B.charAt(0);
-        char ba = Character.toUpperCase(Ba);
-
-        System.out.println(ab + A.substring(1) + " " +ba + B.substring(1) );
-
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i].compareTo(min) < 0) {
+                min = arr[i];
+            }
+        }
+        String max = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i].compareTo(max)>0){
+                max = arr[i];
+            }
+        }
+        System.out.println(min);
+        System.out.println(max);
 
 
 
