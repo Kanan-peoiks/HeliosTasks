a2eadb3b765a0afe4647c0eaed73c2d43652644b
Kanan-peoiks <kenanshukurzadee@gmail.com>
2025-11-27 01:02:09 +0400

    hackerrank

--- src/ferdi/Ferdiishler.java
+++ src/ferdi/Ferdiishler.java
@@ -6,9 +6,37 @@ import java.util.Stack;
 
 public class Ferdiishler {
     public static void main(String[] args) {
-
         Scanner scan = new Scanner(System.in);
-        int n = scan.nextInt();
+        String A = scan.nextLine();
+        String B = scan.nextLine();
+
+        int res = A.length() + B.length();
+        System.out.println(res);
+        int res2 = A.compareTo(B);
+        if ( res2> 0){
+            System.out.println("Yes");
+        }
+        else{
+            System.out.println("No");
+        }
+
+        char Ab = A.charAt(0);
+        char ab = Character.toUpperCase(Ab);
+
+        char Ba = B.charAt(0);
+        char ba = Character.toUpperCase(Ba);
+
+        System.out.println(ab + A.substring(1) + " " +ba + B.substring(1) );
+
+
+
+
+
+
+
+
+
+
 
 
     }
