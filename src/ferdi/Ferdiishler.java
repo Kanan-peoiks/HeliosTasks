5bf4cee97bfefc7744f229796a5296643f0bbe82
Kanan-peoiks <kenanshukurzadee@gmail.com>
2025-11-28 01:24:33 +0400

    hackerrankTask

--- src/ferdi/Ferdiishler.java
+++ src/ferdi/Ferdiishler.java
@@ -11,26 +11,24 @@ import java.util.Stack;
 public class Ferdiishler {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        String s = scan.nextLine().trim();
+    int q = scan.nextInt();
 
-        String[] arr = new String [s.length()];
+        for (int i = 0; i < q; i++) {
 
-        arr = s.split("[ !,?._'@]+");
-        int count=0; //bunu da split etmek olar
+            int a = scan.nextInt();
+            int b = scan.nextInt();
+            int n = scan.nextInt();
+            int res = a;
 
-        for (int i = 0; i < arr.length; i++) {
-            count++;
+
+            for (int j = 1; j <= n; j++) {
+                res = res + b;
+                b = b * 2;
+                System.out.printf(res+" ");
+            }
+            System.out.println();
         }
-
-        System.out.println(count);
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
-        }
-
-
-
-
-
+// 5+1*3 b=3 5+2*3 b6 5+3*6 b18 5+4*18 b72 5+5*72           8,11,21,77,365 -> 8/14/26/50/98
 
 
 
