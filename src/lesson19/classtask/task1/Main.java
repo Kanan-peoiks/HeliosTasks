f7fd2c2cf2a2e2cb8098dd33d7de40b7948bca81
Kanan-peoiks <kenanshukurzadee@gmail.com>
2025-11-30 21:49:39 +0400

    EnumTask1

--- src/lesson19/classtask/task1/Main.java
+++ src/lesson19/classtask/task1/Main.java
@@ -1,4 +1,21 @@
 package lesson19.classtask.task1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+    public static void main(String[] args) {
+        List<Double> list = new ArrayList<>();
+    list.add(30.);
+    list.add(23.111);
+    list.add(123.1);
+
+    for(Double list1 : list){
+        System.out.println(list1);
+    }
+
+
+
+
+    }
 }
