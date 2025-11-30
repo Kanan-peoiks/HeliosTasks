3c626af16e0671feb59fcc8c365bf5ca386ad60c
Kanan-peoiks <kenanshukurzadee@gmail.com>
2025-11-30 22:00:05 +0400

    EnumTask1

--- src/lesson19/classtask/task1/Main.java
+++ src/lesson19/classtask/task1/Main.java
@@ -2,17 +2,33 @@ package lesson19.classtask.task1;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        List<Double> list = new ArrayList<>();
-    list.add(30.);
-    list.add(23.111);
-    list.add(123.1);
+//        List<Double> list = new ArrayList<>();
+//    list.add(30.);
+//    list.add(23.111);
+//    list.add(123.1);
+//
+//    for(Double list1 : list){
+//        System.out.println(list1);
+//    }
 
-    for(Double list1 : list){
-        System.out.println(list1);
-    }
+//----
+        Scanner scan = new Scanner(System.in);
+    ArrayList<Double> dbs = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            Double n = scan.nextDouble();
+            dbs.add(n);
+        }
+        double count = 0;
+        for(Double dbs1 : dbs ){
+            count += dbs1;
+        }
+        System.out.println(count);
+
+
 
 
 
