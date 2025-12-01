b79bcfcc920a087125739b393942ee80917a78b8
Kanan-peoiks <kenanshukurzadee@gmail.com>
2025-12-01 23:56:46 +0400

    .

--- src/lesson19/hometask/HRTaskManagementSystem.java
+++ src/lesson19/hometask/HRTaskManagementSystem.java
@@ -14,6 +14,7 @@ public class HRTaskManagementSystem {
         boolean davam = true;
         TaskStatus status =null;
         UrgencyLevel level = null;
+        Boolean isCritic = true;
         while (davam){
             System.out.println("--- Əsas Menyu ---\n" +
                     "1. Yeni tapşırıq əlavə et\n" +
@@ -22,8 +23,9 @@ public class HRTaskManagementSystem {
                     "4. Tapşırıqları filtrləyərək göstər\n" +
                     "5. Kritik tapşırıqların ümumi saatını hesablasın (Bonus)\n" +
                     "6. Çıxış");
-            int secim = scan.nextInt();
             System.out.println("Seçiminizi daxil edin: ");
+            int secim = scan.nextInt();
+            scan.nextLine();
             switch (secim){
                 case 1:
                     System.out.println("1. Yeni tapşırıq əlavə et. ");
@@ -36,7 +38,7 @@ public class HRTaskManagementSystem {
                     String estimatedHours = scan.nextLine();
 
                     Double estimatedHourss = Double.parseDouble(estimatedHours);
-                    System.out.println("Status daxil edin (PENDING, IN_PROGRESS, ON_HOLD, COMPLETED, ARCHIVED: ");
+                    System.out.println("Status daxil edin (PENDING, IN_PROGRESS, ON_HOLD, COMPLETED, ARCHIVED): ");
                     try{
                         String st = scan.nextLine().toUpperCase();
                         status = TaskStatus.valueOf(st.toUpperCase());}
@@ -51,8 +53,8 @@ public class HRTaskManagementSystem {
                     catch (IllegalArgumentException e){
                         System.out.println("Urgency düzgün seçilməyib.");}
 
-                    System.out.println("Kritikdir? ");
-                    Boolean isCritic = scan.nextBoolean();
+                    System.out.println("Kritikdir? true/false");
+                    isCritic = scan.nextBoolean();
 
                 Task task = new Task(taskId,estimatedHourss,status,level, isCritic);
                 taskList.add(task);
@@ -64,14 +66,16 @@ public class HRTaskManagementSystem {
 
                     System.out.println("TaskId daxil edin: ");
                     Integer newTaskId = scan.nextInt();
+                    scan.nextLine();
                     for (Task t :taskList){ //LISTDE SEARCH EDIRIK
 
                         if(t.getTaskId().equals(newTaskId)){ //EGER VARSA DEYISIKLIYE BASLAYIRIQ
 
-                            System.out.println("Yeni status daxil edin (PENDING, IN_PROGRESS, ON_HOLD, COMPLETED, ARCHIVED: ");
+                            System.out.println("Yeni status daxil edin (PENDING, IN_PROGRESS, ON_HOLD, COMPLETED, ARCHIVED)");
                             // YENI STATUS DAXIL EDIRIK
+                            String yenist = scan.nextLine().toUpperCase();
                             try{
-                                String yenist = scan.nextLine().toUpperCase();
+
                                TaskStatus newStatus = TaskStatus.valueOf(yenist.toUpperCase());
                                //
                             t.updateStatus(newStatus);
@@ -90,27 +94,51 @@ public class HRTaskManagementSystem {
                     }
                     break;
                 case 3:
-                    System.out.println("Tapşırığı Kritik olaraq qeyd et: ");
+                    System.out.println("Tapşırığı Kritik olaraq qeyd et");
                     System.out.println("TaskId daxil edin: ");
                     Integer newTaskIdForCritical = scan.nextInt();
+                    scan.nextLine();
                     for (Task t :taskList){
                         if(t.getTaskId().equals(newTaskIdForCritical)){
-                            isCritic = true;}
+                            isCritic = true;
+                            System.out.println("Tapşırıq kritik olaraq qeyd edildi.");
+                        }
                     }
                     break;
                 case 4:
                     System.out.println("4. Tapşırıqları filtrləyərək göstər (1.Yalnız COMPLETED 2.Yalnız URGENT 3.Yalnız kritik tapşırıqlar 4.Yalnız IN_PROGRESS");
                     Integer secimForFilt = scan.nextInt();
+                    scan.nextLine();
                     Boolean found = false;
                     for (Task t :taskList){
                         switch (secimForFilt){
                             case 1:
+                                System.out.println("Completed uzre filtr: ");
+                                if (t.getStatus()==TaskStatus.COMPLETED){
+                                    System.out.println(t);
+                                    found = true;
+                                }
                                 break;
                             case 2:
+                                System.out.println("Urgent uzre filtr: ");
+                                if (t.getUrgency()==UrgencyLevel.URGENT){
+                                    System.out.println(t);
+                                    found = true;
+                                }
                                 break;
                             case 3:
+                                System.out.println("Kritik uzre filtr: ");
+                                if (t.getCritical()){
+                                    System.out.println(t);
+                                    found = true;
+                                }
                                 break;
                             case 4:
+                                System.out.println("In progress uzre filtr: ");
+                                if (t.getStatus()==TaskStatus.IN_PROGRESS){
+                                    System.out.println(t);
+                                    found = true;
+                                }
                                 break;
                             default:
                                 System.out.println("Yanlış seçim. ");
@@ -122,6 +150,12 @@ public class HRTaskManagementSystem {
                     break;
                 case 5:
                     System.out.println("5. Kritik tapşırıqların ümumi saatını hesablasın: ");
+                    Double count = 0.0;
+                    for (Task t :taskList){
+                        if(isCritic == true){
+                            count += t.getEstimatedHours();}
+                    }
+                    System.out.println(count);
                     break;
                 case 6:
                     System.out.println("6. Çıxış. ");
