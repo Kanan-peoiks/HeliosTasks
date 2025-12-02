6f8de760bb8dc3a8af3b20a5f12a07d2b76dead7
Kanan-peoiks <kenanshukurzadee@gmail.com>
2025-12-02 21:09:51 +0400

    stream task 1

---
+++ src/lesson21/task1/Main.java
@@ -0,0 +1,29 @@
+package lesson21.task1;
+
+import lesson7.task2.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<String> list =new ArrayList<>();
+
+        list.add("maga");
+        list.add("sehriyar");
+        list.add("kerem");
+        list.add("seyfullah");
+        list.add("fidan");
+        list.add("kenan");
+        list.add("rehman");
+        list.add("agaselim");
+
+        List<String> filtered = list.stream()
+                .filter(w -> w.length()>5)
+                .toList();
+        System.out.println(filtered);
+
+
+    }
+}
