c9cb27cd94a7e869bb44e4b6402dccaf56722572
Kanan-peoiks <kenanshukurzadee@gmail.com>
2025-12-28 21:57:03 +0400

    hackerrank 5

--- src/ferdi/Main.java
+++ src/ferdi/Main.java
@@ -3,22 +3,37 @@ package ferdi;
 import java.util.LinkedList;
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        String s = scanner.nextLine();
-
-        try{
-        int n = Integer.parseInt(s);
-            System.out.println(n);
-        } catch (Exception e) {
-            System.out.println("Bad String");
+class Calculator {
+    int power(int n, int p) throws Exception {
+        if (n < 0 || p < 0) {
+            throw new Exception("n and p should be non-negative");
         }
+        int result = 1;
+        for (int i = 0; i < p; i++) {
+            result *= n;
+        }
+        return result;
+    }
+}
 
-
-
-
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Calculator cal = new Calculator();
+        Scanner scan = new Scanner(System.in);
+        int z = scan.nextInt();
+        String[] results = new String[z];
+        for (int i = 0; i < z; i++) {
+            int n = scan.nextInt();
+            int p = scan.nextInt();
+            try {
+                results[i] = String.valueOf(cal.power(n, p));
+            } catch (Exception e) {
+                results[i] = e.getMessage();
+            }
+        }
+        for (String r : results){
+            System.out.println(r);
+        }
 
     }
 }
