99b17f0927fa5a1f06095bf712d1fb27cf77eb21
Kanan-peoiks <kenanshukurzadee@gmail.com>
2025-12-29 00:24:33 +0400

    hackerrank 6.1

--- src/ferdi/Main.java
+++ src/ferdi/Main.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 
 interface AdvancedArithmetic {
     int divisorSum(int n);
+    //İnterface məntiqi budur ki, bu yuxarıda yazdığımız metodu mütləq çağırmalısan deyir
 }
 
 class Calculator implements AdvancedArithmetic {
