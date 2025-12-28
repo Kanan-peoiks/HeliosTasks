1f5a0434a4635e07d37a004b6c2a0dddaba6e2ef
Kanan-peoiks <kenanshukurzadee@gmail.com>
2025-12-28 21:27:47 +0400

    hackerrank 4

--- src/ferdi/Main.java
+++ src/ferdi/Main.java
@@ -5,17 +5,15 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        LinkedList<Integer> list= new LinkedList<>();
-        Scanner scan = new Scanner(System.in);
-        int n =scan.nextInt();
+        Scanner scanner = new Scanner(System.in);
 
-        for (int i = 0; i < n; i++) {
-            int x = scan.nextInt();
-            list.add(x);
-        }
+        String s = scanner.nextLine();
 
-        for (int list1 : list ){
-            System.out.print(list1 + " ");
+        try{
+        int n = Integer.parseInt(s);
+            System.out.println(n);
+        } catch (Exception e) {
+            System.out.println("Bad String");
         }
 
 
