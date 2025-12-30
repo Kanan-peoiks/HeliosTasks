703251119d1b5b5d58e4cdbb0c0e7fc7903e2754
Kanan-peoiks <kenanshukurzadee@gmail.com>
2025-12-30 17:41:25 +0400

    hackerrank 8 generics

--- src/ferdi/Main.java
+++ src/ferdi/Main.java
@@ -1,35 +1,37 @@
 package ferdi;
 
-import java.sql.Array;
-import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
-
 public class Main {
-    public static void main(String[] args){
-    Scanner scan= new Scanner(System.in);
-    int n = scan.nextInt();
-    int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
-        arr[i]= scan.nextInt();
-        }
-        int swapCount = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n-1; j++) {
-                if (arr[j]>arr[j+1]){
-                    int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1]= temp;
-                    swapCount++;
-                }
-            }
-        }
 
-        System.out.println("Array is sorted in "+swapCount+" swaps.");
-        System.out.println("First Element: "+arr[0]);
-        System.out.println("Last Element: "+arr[n-1]);
+    public static <T> void printArray(T [] array){
+        for(T element: array){
+            System.out.println(element);
+        }
+    }
 
-    scan.close();
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        scan.nextLine();
+        Integer[] intArray = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            intArray[i] = scan.nextInt();
+        }
+        int m = scan.nextInt();
+        scan.nextLine();
+        String[] stringArray = new String[m]; // m = element sayı
+        for (int i = 0; i < m; i++) {
+            stringArray[i] = scan.nextLine();
+        }
+        printArray(intArray);
+        printArray(stringArray);
+scan.close();
+
     }
 }
+
 
