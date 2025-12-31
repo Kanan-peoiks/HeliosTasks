c309e30dc841d1d56759c92819c3de2143fc8e42
Kanan-peoiks <kenanshukurzadee@gmail.com>
2025-12-31 23:38:45 +0400

    hackerrank java 1d array

--- src/ferdi/Main.java
+++ src/ferdi/Main.java
@@ -5,39 +5,15 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int n = scan.nextInt();
-        String[] arr = new String[n];
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
-            int k = scan.nextInt();
-            if (k <= 1) {
-                arr[i] = "Not prime";
-            } else {
-                boolean isPrime = true;
-                for (int j = 2; j * j <= k; j++) {
-                    if (k % j == 0) {
-                        isPrime = false;
-                        break;
-                    }
-                }
-                if (isPrime) {
-                    arr[i] = "Prime";
-                } else {
-                    arr[i] = "Not prime";
-                }
-            }
-
+            arr[i] = scanner.nextInt();
         }
 
-        for (String arr1 : arr){
-            System.out.println(arr1);
-        }
-
-
-
-
-
-
+        scanner.close();
     }
 }
 
